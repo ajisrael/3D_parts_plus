0012cabcee61ce06d96e6498937f0b3bbc41890e
Alex Israels <aiuhjv1@amway.com>
2021-06-27 14:07:42 -0400

    f   - setup env vars

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const status = `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`;
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('API is running....');
+  res.send(status);
 });
 
-const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+  console.log(status);
 });
