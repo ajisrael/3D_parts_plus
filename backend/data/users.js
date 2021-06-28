54baf384ea466ad04e3823bad9d4bd1e5cc36f6d
Alex Israels <aiuhjv1@amway.com>
2021-06-28 09:03:47 -0400

    f   - added users to database

---
+++ backend/data/users.js
@@ -0,0 +1,23 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('1234567', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: bcrypt.hashSync('1234567', 10),
+    isManufacturer: true,
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    password: bcrypt.hashSync('1234567', 10),
+  },
+];
+
+export default users;
