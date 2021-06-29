20ef811ff84c9db849f3e53c65a47a97c9e7b876
aiuhjv1 <aiuhjv1@amway.com>
2021-06-29 09:29:11 -0400

    f   - get and update user for admin route created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -131,4 +131,55 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser };
+// @desc    Get user by id
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.isAdmin) {
+      user.isAdmin = true;
+    } else {
+      user.isAdmin = false;
+    }
+
+    if (req.body.isManufacturer) {
+      user.isManufacturer = true;
+    } else {
+      user.isManufacturer = false;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      isManufacturer: user.isManufacturer,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser, getUserById, updateUser };
