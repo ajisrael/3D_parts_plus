233124edbaae91b4495d2736f77eb6970edbe337
aiuhjv1 <aiuhjv1@amway.com>
2021-06-29 14:38:42 -0400

    f   - setup basic landing page

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+
+const HomeScreen = () => {
+
+  return (
+    <>
+      <h1>Welcome to 3D Parts Plus!</h1>
+    </>
+  );
+};
+
+export default HomeScreen;
